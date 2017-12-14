a51627afd5ce6d0736a35d08aee102913f95a35e
jdiazgon <jaime.diaz-gonzalez-external@capgemini.com>
2017-12-14 12:26:14 +0100

    License project first commit

---
+++ src/main/java/com/capgemini/license/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.license;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
